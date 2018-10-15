c16b753b326328285f33922dcf040b22380fea6d
Ahmed Almuhanna <almuhannaa@dewv.edu>
2018-10-15 09:34:20 +0000

    Updates via sync script

--- platformPrincess.js
+++ platformPrincess.js
@@ -2,13 +2,71 @@ import { game, Sprite } from "./sgc/sgc.js";
 game.setBackground("water.png");
 
 class Wall extends Sprite {
-    constructor() {
+    constructor(x, y) {
         super();
         this.name = "Wall";
-        this.setImage("wall.png");
-        this.x = 0;
-        this.y = 175;
+        this.setImage("wall.png", 500, 0);
         this.accelerateOnBounce = false;
+
+    }
+}
+new Wall(0, 175);
+
+class Support extends Sprite {
+    constructor(x, y, image) {
+        super(x, y, image);
+        this.x = x;
+        this.y = y;
+        this.setImage(image);
+    }
+}
+class Platform extends Support {
+    constructor(x, y, image) {
+        super(x, y, image);
+        this.name = "A platform";
+        this.accelerateOnBounce = false;
+    }
+}
+let startPlatform = new Platform(0, 400, "start.png");
+let finishPlatform = new Platform(game.displayWidth - 48 * 2, 400, "finish.png");
+
+class Slider extends Support {
+    constructor(x, y, angle) {
+        super(x, y, "slider.png");
+        this.name = "A sliding support";
+        this.angle = angle;
+        this.speed = 48;
+        this.accelerateOnBounce = true;
+    }
+}
+new Slider(startPlatform.x + 48 * 3, startPlatform.y + 48, 0);
+new Slider(finishPlatform.x - 48 * 5, finishPlatform.y + 48, 180);
+
+class Princess extends Sprite {
+    constructor(x, y, image) {
+        super(48, 300, "ann.png");
+        this.speed = 0;
+        this.speedWhenWalking = 125;
+        this.defineAnimation("left", 9, 11);
+        this.defineAnimation("right", 3, 5);
+
+    }
+    handleLeftArrowKey() {
+        this.angle = 180;
+        this.speed = this.speedWhenWalking;
+    }
+    handleRightArrowKey() {
+        this.angle = 0;
+        this.speed = this.speedWhenWalking;
+    }
+    handleGameLoop() {
+        if(this.angle === 0, this.speed >= 0) {
+            this.playAnimation("right");
+        }
         
+        if(this.angle === 180, this.speed >= 0) {
+            this.playAnimation("left");
+        }
+        this.x = Math.max(5, this.x);
     }
 }
